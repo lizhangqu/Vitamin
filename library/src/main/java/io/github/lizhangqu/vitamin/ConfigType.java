508997e27e64e82ac9de44cbf040877200710420
李樟取 <lizhangqu@weidian.com>
2017-07-19 18:08:56 +0800

    * 枚举

---
+++ library/src/main/java/io/github/lizhangqu/vitamin/ConfigType.java
@@ -0,0 +1,17 @@
+package io.github.lizhangqu.vitamin;
+
+/**
+ * 配置类型
+ *
+ * @author lizhangqu
+ * @version V1.0
+ * @since 2017-07-18 19:39
+ */
+public enum ConfigType {
+    YAML,//.yaml文件
+    INI,//.ini文件
+    XML,//.xml文件
+    JSON,//.json文件
+    PROPERTIES,//.properties文件
+    PREFERENCES,//SharedPreferences文件
+}

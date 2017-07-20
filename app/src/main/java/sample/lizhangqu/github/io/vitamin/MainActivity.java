5be910ec1fba0277a77fec5dfafb6fd7fd1de595
李樟取 <lizhangqu@weidian.com>
2017-07-20 10:33:00 +0800

    * sample

--- app/src/main/java/sample/lizhangqu/github/io/vitamin/MainActivity.java
+++ app/src/main/java/sample/lizhangqu/github/io/vitamin/MainActivity.java
@@ -1,7 +1,20 @@
 package sample.lizhangqu.github.io.vitamin;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import java.io.InputStream;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import io.github.lizhangqu.vitamin.ConfigType;
+import io.github.lizhangqu.vitamin.ReadableConfig;
+import io.github.lizhangqu.vitamin.Vitamin;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +22,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    InputStream inputStream = MainActivity.this.getAssets().open("test.ini");
+                    ReadableConfig config = Vitamin.getInstance().getReadableConfig(ConfigType.INI, inputStream);
+                    List<String> list = config.getList("Keyboard.ROWS");
+                    Log.e("TAG", "ini list:" + list);
+
+                    inputStream = MainActivity.this.getAssets().open("test.yaml");
+                    config = Vitamin.getInstance().getReadableConfig(ConfigType.YAML, inputStream);
+                    list = config.getList("list");
+                    Log.e("TAG", "yaml list:" + list);
+
+                    inputStream = MainActivity.this.getAssets().open("test.xml");
+                    config = Vitamin.getInstance().getReadableConfig(ConfigType.XML, inputStream);
+                    list = config.getList("list");
+                    Log.e("TAG", "xml list:" + list);
+
+                    inputStream = MainActivity.this.getAssets().open("test.json");
+                    config = Vitamin.getInstance().getReadableConfig(ConfigType.JSON, inputStream);
+                    list = config.getList("list");
+                    Log.e("TAG", "json list:" + list);
+
+                    SharedPreferences sharedPreferences = MainActivity.this.getSharedPreferences("test", Context.MODE_PRIVATE);
+                    Set<String> set = new HashSet<String>();
+                    set.add("1");
+                    set.add("2");
+                    set.add("3");
+                    sharedPreferences.edit().putStringSet("list", set).apply();
+
+                    config = Vitamin.getInstance().getReadableConfig(ConfigType.PREFERENCES, "test");
+                    list = config.getList("list");
+                    Log.e("TAG", "sharedPreferences list:" + list);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
